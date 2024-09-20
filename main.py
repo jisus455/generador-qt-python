f6d8f877df73a597851a3041d5344ff7eebd4680
jisus455 <jesus.espi2544@gmail.com>
2024-09-20 20:33:52 -0300

    improvements in code and design

--- main.py
+++ main.py
@@ -6,32 +6,51 @@ from presentation.dialog import Ui_Dialog
 from business.cuilLogic import cuilCalculator
 from business.exportLogic import exportFile
 
+import os
+import os.path
+
 class MyMain(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMain, self).__init__(parent)
         self.setupUi(self)
 
+        self.genero = None
+        self.dni = None
+        self.cuil = None
+
         self.cmbGenero.addItem('Masculino', 'M')
         self.cmbGenero.addItem('Femenino', 'F')
 
         self.btnGenerar.clicked.connect(self.generar)
+        
+        self.actionExportar.setStatusTip("Exportar los datos en un archivo")
         self.actionExportar.triggered.connect(self.ejecutar)
+
     
     
     def generar(self):
-        genero = self.cmbGenero.currentData()
-        dni = self.leDni.text()
-        mycuil = cuilCalculator(dni, genero)
+        self.genero = self.cmbGenero.currentData()
+        self.dni = self.leDni.text()
+        mycuil = cuilCalculator(self.dni, self.genero)
         try:
             mycuil.validate()
-            resultado = mycuil.calculate()
-            self.txtCuil.setText(resultado)
+            self.cuil = mycuil.calculate()
+            self.txtCuil.setText(self.cuil)
         except Exception as e:
             self.txtCuil.setText(e.args[0])
 
     def ejecutar(self):
         dialog = MyDialog()
         dialog.exec()
+         
+        ruta = dialog.data['ruta']
+        data = {'genero': self.genero, 'dni': self.dni, 'cuil': self.cuil}
+        result = exportFile().export(ruta, data)
+        if result:
+            self.statusbar.showMessage("Archivo exportado correctamente", 5000)
+        else:
+            self.statusbar.showMessage("Ocurrio un problema al exportar", 5000)
+
 
         
 class MyDialog(QDialog, Ui_Dialog):
@@ -39,12 +58,35 @@ class MyDialog(QDialog, Ui_Dialog):
         super(MyDialog, self).__init__(parent)
         self.setupUi(self)
 
-        self.btnExportar.clicked.connect(self.exportar)
+        self.data = {}
+        self.ruta = os.getcwd()
+        self.btnAceptar.clicked.connect(self.aceptar)
+        self.btnCancelar.clicked.connect(lambda: self.close())
 
-    def exportar(self):
-        file = exportFile('M', '12345678', '999')
-        file.export()
-        
+        self.btnNueva.clicked.connect(self.nueva)
+        self.btnDescargas.clicked.connect(self.descargas)
+        self.btnDocumentos.clicked.connect(self.documentos)
+        self.btnEscritorio.clicked.connect(self.escritorio)
+
+    def aceptar(self):
+        self.data.setdefault('ruta', self.txtRuta.text())
+        self.close()
+    
+    def nueva(self):
+        ruta = exportFile().getRoute('newdir')
+        self.txtRuta.setText(ruta)
+    
+    def descargas(self):
+        ruta = exportFile().getRoute('downloads')
+        self.txtRuta.setText(ruta)
+
+    def documentos(self):
+        ruta = exportFile().getRoute('documents')
+        self.txtRuta.setText(ruta)
+
+    def escritorio(self):
+        ruta = exportFile().getRoute('desktop')
+        self.txtRuta.setText(ruta)
 
 
 # Inicializar la aplicacion
